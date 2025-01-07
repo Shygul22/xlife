1e6a838183c71c59fc795e6342cd832c17176ffe
Shygul akbar <108211360+Shygul22@users.noreply.github.com>
2025-01-07 23:58:25 +0530

    Delete includes directory

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="bg-gray-800">
-    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <div class="text-center">
-            <p class="text-base text-gray-400">&copy; <?php echo date('Y'); ?> Task Manager. All rights reserved.</p>
-        </div>
-    </div>
-</footer>
-</body>
-</html>
